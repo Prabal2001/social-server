1ab76bccfb35cdde8ac8357d6edbc1b0b5e2a3ce
Prabal Arora <prabalarora2001@gmail.com>
2024-10-14 23:56:46 +0530

    checked for current user

--- src/app/services/jwt.ts
+++ src/app/services/jwt.ts
@@ -1,15 +1,18 @@
 import { User } from "@prisma/client";
-import { prismaClient } from "../../clients/db";
 import JWT from 'jsonwebtoken';
+import { JWTUser } from "../../interfaces";
 const JWT_SECRET = "1$45#$23";
 class JWTService {
      public static async generateTokenForUser(user:User) {
-        const payload = {
+        const payload:JWTUser= {
             id:user?.id,
             email:user?.email,
         }
         const token = JWT.sign(payload,JWT_SECRET);
         return token;
+    }
+    public static decodeToken(token:string) {
+        return JWT.verify(token,JWT_SECRET) as JWTUser
     }
 }   
 
